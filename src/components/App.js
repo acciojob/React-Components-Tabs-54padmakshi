a38d6667551dab5b7bc085b2b5e9b446cacb3c8c
54padmakshi <115454581+54padmakshi@users.noreply.github.com>
2023-04-12 18:42:44 +0530

    not solved yet

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,13 @@
+import React from 'react'
 
+function App() {
+  return (
+    <div className='tabs'>
+      <a href='Home' className='Home-tab'> Home</a>
+      <a href='About'className='About-tab'>About</a>
+      <a href='Features' className='Features-tab'> Features</a>
+    </div>
+  )
+}
+
+export default App
